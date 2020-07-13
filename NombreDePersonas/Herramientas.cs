b719a6db4ff77eba8cc29d9ff21431d6c7b11e20
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-13 00:04:52 -0700

    Agregar archivos de proyecto.

---
+++ NombreDePersonas/Herramientas.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions; //necesaria para expresiones regulares.
+
+namespace NombreDePersonas
+{
+    class Herramientas
+    {
+        //linea de validación con expresiones regulares aceptando letras, espacios,acentos y puntos.
+        Regex condicion = new Regex("^[a-zA-ZÀ-ÿ\\s\\.]*$");
+       
+        //devuelve con estructura ternaria verdadero si la entrada coincide con la validación.
+        public bool ValidacionLetrasEspaciosAcentos(string paraValidar)
+        {
+            //si paraValidar es numero,letra,espacio,acento y diferente de vacío devuelve true
+            return condicion.IsMatch(paraValidar) && paraValidar != string.Empty ? true : false;
+        }
+
+        //Acepta los datos aceptando los parametros de cantidad y tipo de datos a desplegar.
+        public string[] Caputura(string paraCapturar, int elem)
+        {
+            string[] datos = new string[elem];
+            for(int i=0; i<datos.Length; i++)
+            {
+                Console.Clear();
+                Console.WriteLine($"\n\tIngrese el nombre de {elem-i} {paraCapturar} \n\n");
+                Console.Write($"\tPersona {i+1} : ");
+                string persona = Console.ReadLine();
+
+
+                //llama a el metodo "ValidacionLetrasEspaciosAcentos dentro de la misma clase 
+                //pasando la captura del usuario para validarla.
+                if (this.ValidacionLetrasEspaciosAcentos(persona))
+                {
+                    datos[i] = persona;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("\n\tDato invalido, presione cualquier tecla para recapturar");
+                    Console.ReadKey();
+                    Console.ResetColor();
+                    i--; //decrementa el valor de i para forzar la captura del mismo elemento.
+                }
+            }
+
+            return datos; //regresa el arreglo datos.
+            
+        }
+
+        //despliega la lista de personas con un mensaje personalizado
+        public void Desplegar(string[] lista, string mensageParaDesplegar, ConsoleColor color)
+        {
+            Console.Clear();
+            
+            Console.WriteLine(mensageParaDesplegar);
+            Console.ForegroundColor = color;
+            foreach (string elem in lista)
+            {
+                Console.WriteLine("\t"+elem);
+            }
+            Console.ResetColor();
+
+            Console.WriteLine("\n\tPresione cualquier tecla para continuar... Gracias!!!");
+            Console.ReadKey();
+        }
+
+       
+        
+    }
+}
---
+++ NombreDePersonas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
+
+namespace NombreDePersonas
+{
+    //Hacer una aplicación en C# de consola, donde se solicite ingresar nombre
+    //de 10 personas, al terminar de ingresar esas 10 personas, debe de mostrar
+    //una lista con los nombres ingresados.
+
+    //realizada con enfoque a clases.
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Herramientas prueba = new Herramientas(); //instancia de Herramientas (clase personalizada)
+            string[] listaDePersonas = new string[10]; //arreglo de personas
+
+            listaDePersonas = prueba.Caputura("personas", 10); //pasa los argumentos de tipo de dato y cantidad
+                                                               //de datos a capturar.
+
+            //Despliega los datos utilizando los argumentos arreglo,mensaje personalizado y color
+            prueba.Desplegar(listaDePersonas, "\n\tLa lista de personas capturadas es la siguiente: \n",ConsoleColor.Blue);
+
+        }
+
+            
+    }
+}
